eea97326c85802e8681ad63cab49df1853a9e3c6
Gondayao, Jerdan <jdgonzdayao@gmail.com>
2025-03-14 21:15:09 +0800

    chagnes

--- logs/views.py
+++ logs/views.py
@@ -10,6 +10,8 @@ def logs_list(request):
     start_date = request.GET.get('start_date')
     end_date = request.GET.get('end_date')
 
+    logs = Logs.objects.all()
+
     if start_date and end_date:
         logs = logs.filter(datelog__range=[start_date, end_date])
     elif start_date:
@@ -17,4 +19,4 @@ def logs_list(request):
     elif end_date:
         logs = logs.filter(datelog__lte=end_date)
 
-    return render('logs/logs_list.html', {'logs': logs})
+    return render(request, 'logs/logs_list.html', {'logs': logs})
